a8a7b24fa6347e3c7bed2517c4f47c4c6cc66799
prathamesh.pawar <prathamesh.pawar@blancco.com>
2025-04-22 12:05:15 +0530

    Initial Commit

---
+++ SwiftUI_WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI_WeatherApp
+//
+//  Created by Prathamesh Pawar on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
